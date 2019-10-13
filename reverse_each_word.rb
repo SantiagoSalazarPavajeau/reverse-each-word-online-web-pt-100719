88122b8744c8f90da6cb8c1a80d7ae87741f1946
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-10-13 14:12:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,18 @@
 def reverse_each_word(string)
-  string.split(//).reverse
+  # string.split(//).reverse
   # .split(//) can split the string into an array with each letter
+  # .split() can split the sentence into an array with each word
   # .reverse will reverse the order of the elements
   # if each splitted word is a different array it will only reverse each word
   reversed_words= []
   string.split.each do |word|
   reversed_words <<  word.reverse
+  # .split allows the .each to acces each word in the sentence
+  # each word is reversed and added to the new array
+  # the new array now cointains each word reversed but is not a sentence/string
   end
   reversed_words = reversed_words.join(" ")
+  # .join(" ") converts the array elements into a string/sentence and each element is separated by a space
 end
+
+
