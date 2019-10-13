d3b12d256eb1ce1b76daa27c56915ddff4b132b1
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-10-13 14:05:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,11 @@
 def reverse_each_word(string)
   string.split(//).reverse
-  
+  # .split(//) can split the string into an array with each letter
+  # .reverse will reverse the order of the elements
+  # if each splitted word is a different array it will only reverse each word
+  reversed_words= []
+  string.split.each do |word|
+    word.reverse
+  end
+    
 end
