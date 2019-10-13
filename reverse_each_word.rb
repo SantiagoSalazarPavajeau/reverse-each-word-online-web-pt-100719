7cfca867763c48748393a707ade13660b95b1aee
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-10-13 14:16:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -16,8 +16,9 @@ def reverse_each_word(string)
 end
 
 def reverse_each_word(string)
-  string.split.collect do |word|
+  string = string.split
+  string.collect do |word|
   word.reverse
   end
-  string
+  return string
 end
